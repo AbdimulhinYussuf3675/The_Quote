e6444028f07dfd7fd77ea67be1e52ffffc8d59ad
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-05-17 00:46:05 +0300

    added a form to directives

--- src/app/form-quote/form-quote.component.ts
+++ src/app/form-quote/form-quote.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import{Quote} from '../quotes'
 @Component({
   selector: 'app-form-quote',
   templateUrl: './form-quote.component.html',
   styleUrls: ['./form-quote.component.css']
 })
 export class FormQuoteComponent implements OnInit {
+  newQuote = new Quote(0,"","","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+  
+    submitQuote(){
+  this.addQuote.emit(this.newQuote);
+    }
 
   constructor() { }
 
