ce8e19c3ed2585f12c1902b43e991bdfea1f9252
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-05-17 08:20:30 +0300

    added a  form-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +15,12 @@ import { FormQuoteComponent } from './form-quote/form-quote.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import {Quote} from '../quotes'
 export class QuoteComponent implements OnInit {
   quotes: Quote[]=[
     new Quote(1,'BOB MARLEY', 'Don’t gain the world and lose your soul. Wisdom is better than silver and gold.', 'Adam', new Date(2021,7,5)),
-    new Quote(2,'Mohamed Ali', 'Only a man who knows what it is like to be defeated can reach down to the bottom of his soul and come up with the extra ounce of power it takes to win when the match is even.', 'Adam', new Date(2021,2,11)),
-    new Quote(3,'Jack Handey', 'Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.', 'Adam', new Date(2021,2,11))
+    new Quote(2,'MOHAMED ALI', 'Only a man who knows what it is like to be defeated can reach down to the bottom of his soul and come up with the extra ounce of power it takes to win when the match is even.', 'Adam', new Date(2021,2,11)),
+    new Quote(3,'JACK HANDEY', 'Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.', 'Adam', new Date(2021,2,11))
   ];
 
   upVote(index) {
